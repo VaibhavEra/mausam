319249e82a95a82948479eda4a19b574c122c986
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2025-04-10 20:48:35 +0000

    Add InfoBox component to display weather details
    Created a new InfoBox component using Material UI's Card layout to
    present weather information such as temperature, humidity, and
    conditions. Currently uses static data for display. Includes basic
    styling and placeholder image to represent weather visually.

---
+++ src/InfoBox.jsx
@@ -0,0 +1,46 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "./InfoBox.css";
+
+export default function InfoBox() {
+  let info = {
+    city: "Delhi",
+    feelslike: 24,
+    temp: 25,
+    tempMin: 25,
+    tempMax: 25,
+    humidity: 47,
+    weather: "haze",
+  };
+  return (
+    <div className="InfoBox">
+      <h1>Mausam Info</h1>
+      <div className="cardContainer">
+        <Card sx={{ maxWidth: 345 }} className="card">
+          <CardMedia
+            sx={{ height: 140 }}
+            image="https://images.unsplash.com/photo-1680352267694-a7fd4c33d4e1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {info.city} - <em>{info.weather}</em>
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "text.secondary" }}
+              component={"span"}
+            >
+              <p>Temperature: {info.temp}&deg;C</p>
+              <p>Humidity: {info.humidity}%</p>
+              <p>Min Temp: {info.tempMin}&deg;C</p>
+              <p>Max Temp: {info.tempMax}&deg;C</p>
+              <p>Feels Like: {info.feelslike}&deg;C</p>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
